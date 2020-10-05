181a7c900fbe09dc319a003a2d87f6ad900757e3
Jacks5320 <761725455@qq.com>
2020-10-05 21:30:30 +0800

    修复部分排序算法bug

--- src/algorithms/demo2/SortDemo.java
+++ src/algorithms/demo2/SortDemo.java
@@ -1,40 +1,48 @@
 package algorithms.demo2;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 
 public class SortDemo {
     public static void main(String[] args) {
-        int[] arr = {3, 9, 2, 7, 5, -2};
-        System.out.println("排序前：" + Arrays.toString(arr));
-        //冒泡排序
-        //System.out.println("冒泡排序" + Arrays.toString(bubbleSort(arr)));
-        // 选择排序
-        //System.out.println("选择排序：" + Arrays.toString(selectSort(arr)));
-        // 插入排序
-        //System.out.println("插入排序：" + Arrays.toString(insertSort(arr)));
-        // 希尔排序
-        System.out.println("希尔排序" + Arrays.toString(shellSort(arr)));
-    }
-    /*  生成 80000 数据用于排序
-        int[] arrTime = new int[80000];
-        for (int i = 0; i < 80000; i++) {
-            arrTime[i] = (int) (Math.random() * 80000);
+        /* 9 个自定义数据测试排序结果 */
+        //int[] arr = {2, 6, 9, 5, 4, 7, 8, 3, 1};
+        //System.out.println("排序前：" + Arrays.toString(arr));
+        ////System.out.println("冒泡排序" + Arrays.toString(bubbleSort(arr)));
+        ////System.out.println("选择排序：" + Arrays.toString(selectSort(arr)));
+        ////System.out.println("插入排序：" + Arrays.toString(insertSort(arr)));
+        ////System.out.println("希尔排序：" + Arrays.toString(shellSort(arr)));
+        //System.out.println("快速排序：" + Arrays.toString(quickSort(arr, 0, arr.length - 1)));
+
+
+        /* 随机 10 个数据排序 */
+        //int[] arrTime = new int[10];
+        //for (int i = 0; i < 10; i++) {
+        //    arrTime[i] = (int) (Math.random() * 100000);
+        //}
+        //System.out.println("排序前：" + Arrays.toString(arrTime));
+        //System.out.println("排序后：" + Arrays.toString(quickSort(arrTime, 0, arrTime.length - 1)));
+
+        /* 10 w 个数据，测试排序效率 */
+        int[] arrTime = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            arrTime[i] = (int) (Math.random() * 100000);
         }
-        dateTime();
-        //bubbleSort(arrTime);  // 13秒
-        //selectSort(arrTime);    // 4 秒
-        //insertSort(arrTime);  // 1 秒
-        //shellSort(arrTime);  // 7 秒
-        dateTime();
-     */
+        long former = dateTime();
+        //bubbleSort(arrTime);  // 冒泡排序 => 21500 毫秒
+        //selectSort(arrTime);    // 选择排序 => 7500 毫秒
+        //insertSort(arrTime);  // 插入排序 => 1200 毫秒
+        //shellSort(arrTime);  // 希尔排序 => 10w 数据，30 毫秒；100w 数据，250 毫秒 ~ 260 毫秒
+        quickSort(arrTime, 0, arrTime.length - 1);  // 快速排序 => 10w 数据，30毫秒；100w 数据，180 毫秒 ~ 200 毫秒
+        long later = dateTime();
+        System.out.println("时间：" + (later - former) + " 毫秒");
+    }
 
     /* 测试时间 */
-    public static void dateTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:dd:ss.SSSS");
-        Date date = new Date();
-        System.out.println(sdf.format(date));
+    public static long dateTime() {
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:dd:ss.SSSS");
+        //Date date = new Date();
+        //System.out.println(sdf.format(date));
+        return new Date().getTime();
     }
 
     /**
@@ -57,7 +65,6 @@ public class SortDemo {
                     flag = true;  // 优化第二步：如果某一趟有数互相交换，将其设置为 true
                 }
             }
-            //System.out.println("第 " + (i + 1) + " 趟排序结果：" + Arrays.toString(arr));
             if (!flag) {
                 break;  // 优化第三步，如果没有进入交换判断，则交换完毕，直接退出即可
             } else {
@@ -73,99 +80,128 @@ public class SortDemo {
      * @param arr 待排序数组
      */
     public static int[] selectSort(int[] arr) {
-        int minIndex = 0;  // 记录最小值的坐标
-        int temp;  // 交换两个数的中间变量
+        int minIndex = 0;  // 最小值指针
+        int temp = 0;  // 交换中间值
+        boolean flag = false;  // 标志是否有交换
 
-        boolean flag = false;  // 是否有交换
-
-        /* 排序趟数：arr.length - 1 */
-        for (int i = 0; i < arr.length - 1; i++) {
-            // 每次比较从索引 i 处开始，一直比较到 arr.length - 1 处，比较次数为 arr.length - 1 - i
-            for (int j = i; j < arr.length; j++) {
-                if (arr[minIndex] > arr[j]) {
-                    minIndex = j;  // 记录最小值的索引值
+        for (int i = 0; i < arr.length - 1; i++) {  // i 为每轮排序的起始指针
+            minIndex = i;
+            for (int j = i; j < arr.length; j++) {  // j 为每次遍历比较的指针
+                if (arr[minIndex] > arr[j]) {  // 如果当前最小值比遍历到的最小值大就交换
+                    minIndex = j;
                     flag = true;
                 }
             }
-            if (flag) {  // 最小值索引变化了
+            if (flag) {  // 如果有交换，就把 minIndex 与 i 所在的索引交换
                 temp = arr[i];
                 arr[i] = arr[minIndex];
                 arr[minIndex] = temp;
+                flag = false;  // 重置标志
             }
-            flag = false;  // 初始化
         }
         return arr;
     }
 
     /**
-     * 插入排序：
+     * 插入排序：也称直接插入排序
      *
      * @param arr 待排序数组
      */
     public static int[] insertSort(int[] arr) {
-        int insertVal;  // 待插入数
-        int insertIndex;  // 插入位置的前一个位置
-
-        // i = 1，从第二个位置开始
-        for (int i = 1; i < arr.length; i++) {
-            insertVal = arr[i];
+        int insertValue = 0;  // 待插入的值
+        int insertIndex = 0;  // 待插入位置
+        for (int i = 1; i < arr.length; i++) {  // i 为待插入值的位置
+            // 初始化待插入值与待插入位置
+            insertValue = arr[i];
             insertIndex = i - 1;
-            // insertIndex >= 0，保证给 insertVal 找插入位置不越界
-            // insertVal < arr[insertIndex]，表示还没有找到插入位置
-            // 需要将 arr[insertIndex] 后移
-            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
-                arr[insertIndex + 1] = arr[insertIndex];
-                insertIndex--;
+            // insertIndex >= 0 先保证不越界
+            // insertValue < arr[insertIndex]，待插入值比待插入位置的值小时，需要后移 待插入位置的值 和 前移待插入位置指针
+            while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];  // 待插入位置的值后移 1 个位置
+                insertIndex--;  // 待插入位置指针前移 1 个位置
             }
-            // 如果插入位置不是当前位置，才赋值
-            if (insertIndex + 1 != i) {
-                arr[insertIndex + 1] = insertVal;  // 赋值
+            arr[insertIndex + 1] = insertValue;
+        }
+        return arr;
+    }
+
+    /**
+     * 希尔排序：也称递减增量排序算法，增强了直接插入排序，先分组，再进行直接插入排序
+     *
+     * @param arr 待排序数组
+     */
+    public static int[] shellSort(int[] arr) {
+        int insertValue;  // 待插入值
+        int insertIndex;  // 待插入索引
+        // 分组，每组有 gap 个元素，gap 每次缩减 1 / 2
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第 gap 个元素开始，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                // 初始化待插入值与待插入位置
+                insertValue = arr[i];
+                insertIndex = i - gap;
+                // insertIndex >= 0 保证不越界
+                // insertValue < arr[insertIndex]，待插入值比待插入位置的值小时，需要后移 待插入位置的值 和 前移待插入位置指针
+                while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
+                    arr[insertIndex + gap] = arr[insertIndex];  // 待插入位置的值后移 gap 个位置
+                    insertIndex -= gap;  // 待插入位置指针前移 gap 个位置
+                }
+                // while 循环完后，insertIndex + gap 就是待插入位置
+                arr[insertIndex + gap] = insertValue;
             }
         }
         return arr;
     }
 
     /**
-     * 希尔排序
+     * 快速排序：对冒泡排序的一种改进
      *
-     * @param arr 待排序数组
+     * @param arr   待排序数组
+     * @param left  左指针
+     * @param right 右指针
+     * @return 排序完成的数组
      */
-    public static int[] shellSort(int[] arr) {
-        // 增量 gap， 并逐步的减小增量
-        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < arr.length; i++) {
-                int j = i;
-                int temp = arr[j];
-                if (arr[j] < arr[j - gap]) {
-                    while (j - gap >= 0 && temp < arr[j - gap]) {
-                        // 移动
-                        arr[j] = arr[j - gap];
-                        j -= 1;
-                    }
-                    // while 退出就找到插入位置了
-                    arr[j] = temp;
-                }
+    public static int[] quickSort(int[] arr, int left, int right) {
+        int l = left;  // 左指针
+        int r = right;  // 右指针
+        int pivot = arr[(left + right) / 2];  // 基准值，总是以 中间值 作为基准值
+        int temp;
+        while (l < r) {  // 当左指针小于右指针时，继续执行。
+            while (arr[l] < pivot) {  // 从左往右找到比 pivot 大的值
+                l++;
+            }
+            while (arr[r] > pivot) {  // 从右往左找到比 pivot 小的值
+                r--;
+            }
+            if (l >= r) {  // 如果 l >= r ，说明左边的值总是小于 pivot ，右边的值总是大于 pivot
+                break;
+            } else {  // 否则交换两个值
+                temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+            // 交换完后，发现 arr[l] == pivot ，r 前移
+            if (arr[l] == pivot) {
+                r--;
+            }
+            // 交换完后，发现 arr[r] == pivot ，l 后移
+            if (arr[r] == pivot) {
+                l++;
             }
         }
-
-    /*
-        交换法
-        int temp;  // 交换临时变量
-        // 代码整理
-        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < arr.length; i++) {
-                for (int j = i - gap; j >= 0; j -= gap) {
-                    // 如果当前元素大于加上步长后的那个元素，说明交换
-                    if (arr[j] > arr[j + gap]) {
-                        temp = arr[j];
-                        arr[j] = arr[j + gap];
-                        arr[j + gap] = temp;
-                    }
-                }
-            }
+        // 如果 l = r，必须 l++，r--，否则栈溢出
+        if (l == r) {
+            l += 1;
+            r -= 1;
         }
-   */
+        // 向左递归
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        // 向右递归
+        if (l < right) {
+            quickSort(arr, l, right);
+        }
         return arr;
     }
-
 }
